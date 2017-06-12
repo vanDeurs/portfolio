f6aa8d8692ae7c6087e6d872c906a43d88be12bf
Alexander Wilson van Deurs <alexwvd@outlook.com>
2017-06-12 18:00:51 +0200

    PHP added.

--- resources/index.php
+++ resources/index.php
@@ -8,10 +8,18 @@ $subject = 'Hello';
 
 $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 if ($_POST['submit']) {
-    if (mail ($to, $subject, $body, $from)) { 
-        echo '<p>Your message has been sent!</p>';
-    } else { 
-        echo '<p>Something went wrong, go back and try again!</p>'; 
+    if ($name != '' && $email != '') {
+        if ($human == '4') {				 
+            if (mail ($to, $subject, $body, $from)) { 
+	        echo '<p>Your message has been sent!</p>';
+	    } else { 
+	        echo '<p>Something went wrong, go back and try again!</p>'; 
+	    } 
+	} else if ($_POST['submit'] && $human != '4') {
+	    echo '<p>You answered the anti-spam question incorrectly!</p>';
+	}
+    } else {
+        echo '<p>You need to fill in all required fields!!</p>';
     }
 }
 ?>
