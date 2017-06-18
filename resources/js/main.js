f9bcc20ca33a1d343bf9aedeb281680cc1c7800e
Alexander Wilson van Deurs <alexwvd@outlook.com>
2017-06-18 21:33:45 +0200

    Added landing image slider, remade landing page, changed colorscheme.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,4 @@
-// Sliding down animation on a single page 
+// Sliding down and up when clicking to different parts of the page
 
 $('a[href^="#"]').click(function () {
 
@@ -9,3 +9,17 @@ var the_id = $(this).attr("href");
     }, 'slow');
 
 return false;});
+
+// Carousel function
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
